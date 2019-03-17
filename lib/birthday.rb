62de42555523682aeb06e4d89af91849ae0916ee
Sam kuriakose <samkuriakose26@gmail.com>
2019-03-17 20:43:16 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,8 +14,6 @@ def age_appropriate_birthday(birthday_hash)
   birthday_hash.each do |kids_name, age|
   if age <= 12
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-end
-  end
 
   end
 
