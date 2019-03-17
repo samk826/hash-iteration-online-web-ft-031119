0863d67857630565141f9ad3abd21ddadd44b022
Sam kuriakose <samkuriakose26@gmail.com>
2019-03-17 20:30:24 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,7 +10,7 @@ def happy_birthday(birthday_kids)
   end
 end
 
-def age_appropriate_birthday(age)
+def age_appropriate_birthday(kids_name, age)
   if
     "age" <12
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
